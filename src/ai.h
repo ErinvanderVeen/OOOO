99972c74db857a9d33bc21616ea9476bd3022054
Erin van der Veen <erinvanderveen@outlook.com>
2019-04-12 11:01:52 +0200

    Combine state into a single type (#27)
    Solves #10

--- src/ai.h
+++ src/ai.h
@@ -24,10 +24,10 @@ uint8_t count_pieces(uint64_t board) {
  *
  * @param[in,out] The coordinate of a valid move (0 - 63) or 64 in case no more valid moves exist
  */
-void next_possible_move(uint64_t ai_player_b, uint64_t ai_opponent_b, uint64_t to_flip[8][8], uint8_t *coordinate) {
+void next_possible_move(board_t board, uint64_t to_flip[8][8], uint8_t *coordinate) {
 	while (*coordinate < 64) {
 		//TODO: Replace column, row with a single coordinate. Related to issue #10
-		if (is_valid_move(ai_player_b, ai_opponent_b, *coordinate % 8, *coordinate / 8, to_flip)) {
+		if (is_valid_move(board, *coordinate % 8, *coordinate / 8, to_flip)) {
 			return;
 		}
 		(*coordinate)++;
@@ -36,9 +36,9 @@ void next_possible_move(uint64_t ai_player_b, uint64_t ai_opponent_b, uint64_t t
 }
 
 // Placeholder evaluation function
-double evaluation(uint64_t ai_player_b, uint64_t ai_opponent_b) {
-	double c_player = (double) count_pieces(ai_player_b);
-	double c_opponent = (double) count_pieces(ai_opponent_b);
+double evaluation(board_t board) {
+	double c_player = (double) count_pieces(board.player);
+	double c_opponent = (double) count_pieces(board.opponent);
 
 	return c_player - c_opponent;
 }
@@ -48,10 +48,10 @@ double evaluation(uint64_t ai_player_b, uint64_t ai_opponent_b) {
  *
  * TODO: to_flip can be passed to this function such that we don't have to allocate
  */
-double alphabeta(uint64_t ai_player_b, uint64_t ai_opponent_b, uint64_t depth, double alpha, double beta, bool maximize) {
+double alphabeta(board_t board, uint64_t depth, double alpha, double beta, bool maximize) {
 	debug_print("Depth: %" PRIu64 "\n", depth);
 	if (depth == 0)
-		return evaluation(ai_player_b, ai_opponent_b);
+		return evaluation(board);
 
 	uint64_t to_flip[8][8];
 
@@ -60,51 +60,55 @@ double alphabeta(uint64_t ai_player_b, uint64_t ai_opponent_b, uint64_t depth, d
 	uint8_t move = 0;
 
 	if (maximize) {
-		next_possible_move(ai_player_b, ai_opponent_b, to_flip, &move);
+		next_possible_move(board, to_flip, &move);
 		val = -INFINITY;
 
 		while (move != 64) {
-			uint64_t new_ai_player_b = ai_player_b;
-			uint64_t new_ai_opponent_b = ai_opponent_b;
+			board_t new_board = {
+				.player = board.player,
+				.opponent = board.opponent
+			};
 
 			// Update board states
-			do_move(&new_ai_player_b, &new_ai_opponent_b, move % 8, move / 8, to_flip);
+			do_move(&board, move % 8, move / 8, to_flip);
 
-			val = fmax(val, alphabeta(new_ai_player_b, new_ai_opponent_b, depth - 1, alpha, beta, false));
+			val = fmax(val, alphabeta(new_board, depth - 1, alpha, beta, false));
 			alpha = fmax(alpha, val);
 
 			if (alpha >= beta)
 				break;
 
 			move++;
-			next_possible_move(ai_player_b, ai_opponent_b, to_flip, &move);
+			next_possible_move(board, to_flip, &move);
 		}
 		return val;
 	} else {
-		next_possible_move(ai_opponent_b, ai_player_b, to_flip, &move);
+		next_possible_move(board, to_flip, &move);
 		val = INFINITY;
 
 		while (move != 64) {
-			uint64_t new_ai_player_b = ai_player_b;
-			uint64_t new_ai_opponent_b = ai_opponent_b;
+			board_t new_board = {
+				.player = board.player,
+				.opponent = board.opponent
+			};
 
 			// Update board states
-			do_move(&new_ai_opponent_b, &new_ai_player_b, move % 8, move / 8, to_flip);
+			do_move(&board, move % 8, move / 8, to_flip);
 
-			val = fmin(val, alphabeta(new_ai_player_b, new_ai_opponent_b, depth - 1, alpha, beta, true));
+			val = fmin(val, alphabeta(new_board, depth - 1, alpha, beta, true));
 			beta = fmin(beta, val);
 
 			if (alpha > beta)
 				break;
 
 			move++;
-			next_possible_move(ai_opponent_b, ai_player_b, to_flip, &move);
+			next_possible_move(board, to_flip, &move);
 		}
 		return val;
 	}
 }
 
-uint8_t ai_turn(uint64_t ai_player_b, uint64_t ai_opponent_b) {
+uint8_t ai_turn(board_t board) {
 	uint64_t to_flip[8][8];
 	uint8_t best_move = 0;
 	double best_val = -INFINITY;
@@ -112,26 +116,28 @@ uint8_t ai_turn(uint64_t ai_player_b, uint64_t ai_opponent_b) {
 	uint8_t move = 0;
 	double val;
 
-	next_possible_move(ai_player_b, ai_opponent_b, to_flip, &move);
+	next_possible_move(board, to_flip, &move);
 
 	while (move != 64) {
 
 		// We don't make a copy of to_flip, because we don't have to
 		// it is updated after every next_possible_move anyway
-		uint64_t new_ai_player_b = ai_player_b;
-		uint64_t new_ai_opponent_b = ai_opponent_b;
+		board_t new_board = {
+			.player = board.player,
+			.opponent = board.opponent
+		};
 
 		// Update board states
-		do_move(&new_ai_player_b, &new_ai_opponent_b, move % 8, move / 8, to_flip);
+		do_move(&board, move % 8, move / 8, to_flip);
 
 		// Perform alphabeta on all children
-		if ((val = alphabeta(new_ai_player_b, new_ai_opponent_b, 8, -INFINITY, INFINITY, false)) > best_val) {
+		if ((val = alphabeta(new_board, 8, -INFINITY, INFINITY, false)) > best_val) {
 			best_move = move;
 			best_val = val;
 		}
 
 		move++;
-		next_possible_move(ai_player_b, ai_opponent_b, to_flip, &move);
+		next_possible_move(board, to_flip, &move);
 	}
 
 	return best_move;
--- src/state_t.h
+++ src/state_t.h
@@ -13,10 +13,12 @@ typedef struct {
 } coordinate_t;
 
 /**
- * Initializes the default state of Othello
+ * Holds the most common way the board state is represented
  */
-void init_state(void) {
-}
+typedef struct {
+	uint64_t player;
+	uint64_t opponent;
+} board_t;
 
 /**
  * Checks if the specified board has a piece in the specified location
@@ -81,13 +83,13 @@ void set_valid_move(uint64_t *valid_moves, uint8_t column, uint8_t row, bool val
  * @param[in] The row
  * @param[in] Wether or not valid moves should be highlighted
  */
-void print_line(uint64_t player_b, uint64_t opponent_b, uint64_t valid_moves, uint8_t y, bool show_valid_moves) {
+void print_line(board_t board, uint64_t valid_moves, uint8_t y, bool show_valid_moves) {
 	printf("%" PRIu8 " ", y + 1);
 	for (uint8_t x = 0; x < 8; x++) {
 		printf("\u2502");
-		if (is_piece(player_b, x, y)) {
+		if (is_piece(board.player, x, y)) {
 			printf("\u26AA");
-		} else if (is_piece(opponent_b, x, y)) {
+		} else if (is_piece(board.opponent, x, y)) {
 			printf("\u26AB");
 		} else if (is_piece(valid_moves, x, y) && show_valid_moves) {
 			printf("\u25A1 ");
@@ -103,13 +105,13 @@ void print_line(uint64_t player_b, uint64_t opponent_b, uint64_t valid_moves, ui
  *
  * @param[in] Wether or not valid moves should be highlighted
  */
-void print_state(uint64_t player_b, uint64_t opponent_b, uint64_t valid_moves, bool show_valid_moves) {
+void print_state(board_t board, uint64_t valid_moves, bool show_valid_moves) {
 	// Duplicate horizontal bars because our pieces are double-width
 	printf("\n  \u250C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u252C\u2500\u2500\u2510\n");
-	print_line(player_b, opponent_b, valid_moves, 0, show_valid_moves);
+	print_line(board, valid_moves, 0, show_valid_moves);
 	for (uint8_t y = 1; y < 8; y++) {
 		printf("  \u251C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u253C\u2500\u2500\u2524\n");
-	print_line(player_b, opponent_b, valid_moves, y, show_valid_moves);
+	print_line(board, valid_moves, y, show_valid_moves);
 	}
 	printf("  \u2514\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2534\u2500\u2500\u2518\n");
 	printf("   a  b  c  d  e  f  g  h\n");
@@ -122,9 +124,9 @@ void print_state(uint64_t player_b, uint64_t opponent_b, uint64_t valid_moves, b
  * @param[in,out] The opponents board
  * @param[in] The mask of the pieces to be flipped
  */
-void flip_neighbours(uint64_t *player_b, uint64_t *opponent_b, uint64_t flip_mask) {
-	*player_b |= flip_mask;
-	*opponent_b &= *player_b ^ UINT64_MAX;
+void flip_neighbours(board_t *board, uint64_t flip_mask) {
+	board->player |= flip_mask;
+	board->opponent &= board->player ^ UINT64_MAX;
 }
 
 /**
@@ -134,17 +136,17 @@ void flip_neighbours(uint64_t *player_b, uint64_t *opponent_b, uint64_t flip_mas
  * @param[in] The column of the desired location
  * @param[in] The row of the desired location
  */
-void do_move(uint64_t *player_b, uint64_t *opponent_b, uint8_t column, uint8_t row, uint64_t to_flip[8][8]) {
+void do_move(board_t *board, uint8_t column, uint8_t row, uint64_t to_flip[8][8]) {
 	debug_print("Placing piece at: %c%" PRIu8 "\n", column + 97, row + 1);
 
-	if (is_piece(*player_b | *opponent_b, column, row)) {
+	if (is_piece(board->player | board->opponent, column, row)) {
 		printf("ERROR: Tried to place piece on occupied field\n");
 		exit(EXIT_FAILURE);
 	}
 
-	place_piece(player_b, column, row);
+	place_piece(&board->player, column, row);
 
-	flip_neighbours(player_b, opponent_b, to_flip[column][row]);
+	flip_neighbours(board, to_flip[column][row]);
 }
 
 /**
@@ -154,8 +156,8 @@ void do_move(uint64_t *player_b, uint64_t *opponent_b, uint8_t column, uint8_t r
  * @param[in] The column of the desired location
  * @param[in] The row of the desired location
  */
-bool is_valid_move(uint64_t player_b, uint64_t opponent_b, uint8_t column, uint8_t row, uint64_t to_flip[8][8]) {
-	if (is_piece(player_b | opponent_b, column, row))
+bool is_valid_move(board_t board, uint8_t column, uint8_t row, uint64_t to_flip[8][8]) {
+	if (is_piece(board.player | board.opponent, column, row))
 		return false;
 
 	bool is_valid = false;
@@ -165,17 +167,17 @@ bool is_valid_move(uint64_t player_b, uint64_t opponent_b, uint8_t column, uint8
 		for(int8_t x = -1; x <= 1; x++) {
 			int8_t xx = column + x;
 			int8_t yy = row + y;
-			if((x == 0 && y == 0) || !is_piece(opponent_b, xx, yy))
+			if((x == 0 && y == 0) || !is_piece(board.opponent, xx, yy))
 				continue;
 
 			do {
 				xx += x;
 				yy += y;
-				if (!is_piece(opponent_b, xx, yy))
+				if (!is_piece(board.opponent, xx, yy))
 					break;
 			} while (xx < 8 && xx >= 0 && yy < 8 && yy >= 0);
 
-			if (is_piece(player_b, xx, yy)) {
+			if (is_piece(board.player, xx, yy)) {
 				for (; xx != column || yy != row; xx -= x, yy -= y) {
 					to_flip[column][row] |= (uint64_t) 1 << ((7 - xx) + ((7 - yy) * 8));
 				}
@@ -190,11 +192,11 @@ bool is_valid_move(uint64_t player_b, uint64_t opponent_b, uint8_t column, uint8
  * Updates the valid_move bitboard. By checking for every square if it would
  * be a valid move.
  */
-void update_valid_moves(uint64_t player_b, uint64_t opponent_b, uint64_t *valid_moves, uint64_t to_flip[8][8], coordinate_t possible_moves[POSSIBLE_MOVES_MAX]) {
+void update_valid_moves(board_t board, uint64_t *valid_moves, uint64_t to_flip[8][8], coordinate_t possible_moves[POSSIBLE_MOVES_MAX]) {
 	uint8_t nr_possible_moves = 0;
 	for (uint8_t column = 0; column < 8; column++) {
 		for (uint8_t row = 0; row < 8; row++) {
-			bool valid = is_valid_move(player_b, opponent_b, column, row, to_flip);
+			bool valid = is_valid_move(board, column, row, to_flip);
 			set_valid_move(valid_moves, column, row, valid);
 			if (valid) {
 				possible_moves[nr_possible_moves].column = column;
