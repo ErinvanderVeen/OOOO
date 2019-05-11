47027d9628ce721d8d43ac4e56ec627f843d5213
Erin van der Veen <erinvanderveen@outlook.com>
2019-05-11 13:49:23 +0200

    Reformat JSON

--- comment_pr.sh
+++ comment_pr.sh
@@ -1,7 +1,8 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
 	make clean -C src
 	make metrics -C src
-	OUTPUT="$(./src/othello.out)"
+	OUTPUT="$(./src/othello.out | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')"
+	echo "{\"body\": \"$OUTPUT\"}"
 	curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
 	-d "{\"body\": \"$OUTPUT\"}" \
 	"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
